06a8b7ec4edd91d621d1719464ba75d79a4ccef9
d61ni4 <butus4232@mail.ru>
2023-03-23 00:34:19 +1000

    dungeon bsp generation WIP

--- DungeonGeneration.cs
+++ DungeonGeneration.cs
@@ -34,6 +34,58 @@ class Tree {
     public Tree(Container leaf) {
         this.leaf = leaf;
     }
+
+    public Tree Split(int numberOfIterations, Container container)
+{
+    Tree node = new Tree(container); 
+
+    if (numberOfIterations == 0)
+    {
+        return node;
+    }
+
+    Container[] splitedContainer = SplitContainer(container);
+    node.Lchild = Split(numberOfIterations - 1, splitedContainer[0]);
+
+    node.Rchild = Split(numberOfIterations - 1, splitedContainer[1]);
+
+    return node;
+}
+
+    private Container[] SplitContainer(Container container)
+    {
+    Container c1, c2;
+    int horizontal = rnd.Next(0, 1);
+    if (horizontal == 1)
+    {
+        c1 = new Container(container.Xcord, container.Ycord, (int)(container.Width * (rnd.Next(30, 60) / 100)), container.Height);
+        c2 = new Container(container.Xcord + c1.Width, container.Ycord, container.Width - c1.Width, container.Height);
+    }
+    else
+    {
+        c1 = new Container(container.Xcord, container.Ycord, container.Width, (int)(container.Height * (rnd.Next(30, 60) / 100)));
+        c2 = new Container(container.Xcord, container.Ycord + c1.Height, container.Width, container.Height - c1.Height);
+    }
+    return new Container[] { c1, c2 };
+    }
+    public void Paint() {
+        Console.SetCursorPosition(this.leaf.Xcord, this.leaf.Ycord);
+        for (int i = 0; i < this.leaf.Height; i++) {
+            for (int j = 0; j < this.leaf.Width; j++){
+                if ((i == 0) || (j == 0) || (i == this.leaf.Height - 1) || (j == this.leaf.Width - 1)) {
+                    Console.Write('#');
+                } else {
+                    Console.Write('.');
+                }
+            }
+             Console.SetCursorPosition(this.leaf.Xcord, this.leaf.Ycord+i+1);
+        }
+        if (this.Lchild != null) {
+            this.Lchild.Paint();
+        } else {}
+        if (this.Rchild != null) this.Rchild.Paint();
+
+    }
 }
 
 class Container {
--- Roguelike.cs
+++ Roguelike.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 class Program {
     public static void Main(string[] args) {
-   
+        Console.Clear();
+        Tree dungeon = new Tree(new Container(1, 1, 10, 5));
+        dungeon = dungeon.Split(5, new Container(1, 1, 100, 50));
+        dungeon.Paint();
     }
 }
